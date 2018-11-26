182a843a0da20eedb755e751f6b90e3a13b07856
raffll <rafal.wierzchos@gmail.com>
2018-11-26 18:05:55 +0100

    in progress

--- app/src/main/java/com/braindead/scoreboard/utilities/BindingAdapter.java
+++ app/src/main/java/com/braindead/scoreboard/utilities/BindingAdapter.java
@@ -1,6 +1,7 @@
 package com.braindead.scoreboard.utilities;
 
 import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,5 +22,13 @@ public abstract class BindingAdapter {
     @android.databinding.BindingAdapter("android:visibility")
     public static void setVisibility(View view, Boolean value) {
         view.setVisibility(value ? View.VISIBLE : View.GONE);
+    }
+
+    @android.databinding.BindingAdapter("android:background")
+    public static void setBackground(TextView view, int color) {
+        GradientDrawable shape = new GradientDrawable();
+        shape.setShape(GradientDrawable.OVAL);
+        shape.setColor(color);
+        view.setBackground(shape);
     }
 }
