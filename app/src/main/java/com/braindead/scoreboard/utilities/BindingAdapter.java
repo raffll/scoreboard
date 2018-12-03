f614a8663760bb3bd929eaff941615a6f0a03548
raffll <rafal.wierzchos@gmail.com>
2018-12-03 13:22:28 +0100

    in progress

--- app/src/main/java/com/braindead/scoreboard/utilities/BindingAdapter.java
+++ app/src/main/java/com/braindead/scoreboard/utilities/BindingAdapter.java
@@ -8,35 +8,23 @@ import android.widget.TextView;
 
 public abstract class BindingAdapter {
 
-    @android.databinding.BindingAdapter("android:textStyle")
-    public static void setTypeface(TextView view, String style) {
-        switch (style) {
-            case "bold":
-                view.setTypeface(null, Typeface.BOLD);
-                break;
-            default:
-                view.setTypeface(null, Typeface.NORMAL);
-                break;
-        }
+    @android.databinding.BindingAdapter("bind:textStyle")
+    public static void setTypeface(TextView view, Boolean value) {
+        view.setTypeface(null, value ? Typeface.BOLD : Typeface.NORMAL);
     }
 
-    @android.databinding.BindingAdapter("android:visibility")
+    @android.databinding.BindingAdapter("bind:visibility")
     public static void setVisibility(View view, Boolean value) {
         view.setVisibility(value ? View.VISIBLE : View.GONE);
     }
 
-    @android.databinding.BindingAdapter("android:background")
+    @android.databinding.BindingAdapter("bind:background")
     public static void setBackground(TextView view, int color) {
         GradientDrawable shape = new GradientDrawable();
         shape.setShape(GradientDrawable.OVAL);
         shape.setColor(color);
         view.setBackground(shape);
     }
-
-    /*@android.databinding.BindingAdapter("android:backgroundTint")
-    public static void setBackgroundTint(View view, int color) {
-        view.setBackgroundTintList(ColorStateList.valueOf(color));
-    }*/
 
     @android.databinding.BindingAdapter({"bind:textSize"})
     public static void setTextSize(TextView view, int value) {
